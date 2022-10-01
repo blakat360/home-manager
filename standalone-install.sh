a69e2ee617f46cadb09e7b0345512b7f6845e141
sigkill <blakat360@gmail.com>
2022-10-02 00:00:07 +0100

    hello world

---
+++ standalone-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# install nix
+sh <(curl -L https://nixos.org/nix/install) --daemon
+export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}
+# install home manager
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
+nix-shell '<home-manager>' -A install
