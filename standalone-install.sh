a835e8c34d4722bf3e8476b2f1997681e6992838
sigkill <blakat360@gmail.com>
2022-10-02 15:57:07 +0100

    updated standalone install

--- standalone-install.sh
+++ standalone-install.sh
@@ -2,8 +2,14 @@
 
 # install nix
 sh <(curl -L https://nixos.org/nix/install) --daemon
+# need to restart shell to make env vars show up
+bash
 export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}
 # install home manager
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 nix-shell '<home-manager>' -A install
+# enable flake support
+echo "experimental-features = nix-command flakes" | sudo tee -a /etc/nix/nix.conf
+# switch to the config
+home-manager switch --flake 'flake.nix#sigkill'
